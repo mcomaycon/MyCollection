b4af2e4c5cc6530a8e1655ab624764b02e0fb884
mcomaycon <82045987+mcomaycon@users.noreply.github.com>
2021-05-19 20:55:45 -0300

    'AlteraCab'

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div>
-            <h1>JU EU TE AMO!!!</h1>
+            <h1>SEJA BEM VINDO!!!!</h1>
             <h2>BORA TOMAR CERVEJA?</h2>
             <h2>Ass: Maycon Cesar de Oliveira</h2>
         </div>)
