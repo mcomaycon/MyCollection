0194ef04cc42d2af8f0d1ca185be6d703e910dac
mcomaycon <82045987+mcomaycon@users.noreply.github.com>
2021-05-19 18:39:24 -0300

    'NovoTexto'

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ function Home() {
     return (
         <div>
             <h1>JU EU TE AMO!!!</h1>
+            <h2>BORA TOMAR CERVEJA?</h2>
             <h2>Ass: Maycon Cesar de Oliveira</h2>
         </div>)
     
