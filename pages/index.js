29d0561c354d8fba4501df2bc1b23a79e1f0e242
mcomaycon <82045987+mcomaycon@users.noreply.github.com>
2021-07-01 12:58:03 -0300

    Resolvendo Erro

--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,11 @@ function Home() {
     return (
         <div>
             <h1>SEJA BEM VINDO!!!!</h1>
-            <Busca />
         </div>
         
     )
 }
 
-async function Busca() {
-    const bd = require("./bd")
-    const Clientes = await bd.BucaCli();
 
-    return (
-        <div>
-            <Busca />
-        </div>
-        )
-}
 
 export default Home
