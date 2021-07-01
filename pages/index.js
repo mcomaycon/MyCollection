3b8efa389eacfa80c8170479e0563bb2a353b463
mcomaycon <82045987+mcomaycon@users.noreply.github.com>
2021-07-01 13:06:31 -0300

    Resolvendo Erro

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
 function Home() {
-    
-
     return (
         <div>
-            <h1>SEJA BEM VINDO!!!!</h1>
+            <h1>SEJA BEM VINDO</h1>
         </div>
-        
     )
 }
-
-
-
 export default Home
